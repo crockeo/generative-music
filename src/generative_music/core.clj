c49f48d4320567c067d00ce2d78a481da8ce33ae
Crockeo <crockeo@gmail.com>
2014-11-21 16:52:13 -0500

    Added functions to emulate strumming the strings at slightly varying points in time.

--- src/generative_music/core.clj
+++ src/generative_music/core.clj
@@ -7,7 +7,7 @@
   (midi->hz (note music-note)))
 
 ;; A synth meant to sound like a guitar.
-(definst guitar [frequency 440 duration 15
+(definst guitar [frequency 440 duration 10
                  h0 1 h1 0.5 h2 0.3 h3 0.25 h4 0.2 h5 0.16 h6 0.14]
   (let [harmonic-series [ 1  2  3  4  5  6  7]
         proportions     [h0 h1 h2 h3 h4 h5 h6]
@@ -20,19 +20,36 @@
     (detect-silence whole :action FREE)
     whole))
 
+;; A macro to simulate strumming down on a guitar.
+(defn strum-down [notes speed]
+  (let [origin (now)
+        l (count notes)]
+    (loop [index 0]
+      (if (>= index l)
+        nil
+        (do
+          (at (+ origin (* speed index)) (guitar (get notes index)))
+          (recur (+ index 1)))))))
+
+;; A macro to simulate strumming up on a guitar.
+(defn strum-up [notes speed]
+  (let [rnotes (into [] (reverse notes))
+        origin (now)
+        l (count rnotes)]
+    (loop [index 0]
+      (if (>= index l)
+        nil
+        (do
+          (at (+ origin (* speed index)) (guitar (get rnotes index)))
+          (recur (+ index 1)))))))
+
 ;; Playing a chord on the guitar.
 (defn guitar-chord [notes]
   (map guitar notes))
 
 ;; Playing a chord on the guitar at a given point in time.
 (defn guitar-chord-at [[time notes]]
-  (let [l (count notes)]
-    (loop [index 0]
-      (if (>= index l)
-        nil
-        (do
-          (at time (guitar (get notes index)))
-          (recur (+ index 1)))))))
+  (map (fn [note] (at time (guitar note))) notes))
 
 ;; Playing a whole song on the guitar.
 (defn guitar-song [song]
